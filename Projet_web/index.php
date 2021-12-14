3a4807e1d19a6e260c9bb5d38feec1c2512c26fe
Cyrellema <92728229+Cyrellema@users.noreply.github.com>
2021-12-14 11:27:48 +0100

    Update index.php

--- Projet_web/index.php
+++ Projet_web/index.php
@@ -13,16 +13,7 @@ $dsn = 'mysql:host=localhost;dbname=projet_web;charset=UTF8';
         <br>
 
         Password
-        <input type="motDePasse" id="mdp" class="motDePasse" name="motDePasse" onkeypress="return runScript(event)"  placeholder="password"/>
+        <input type="motDePasse" id="mdp" class="motDePasse" name="motDePasse" placeholder="password"/>
         <input type="submit" id="valider" class="validez" value="validez" >
-        <script>
-          document.getElementById("mdp")
-          .addEventListener("keyup", function(event){
-             event.preventDefault();
-             if(event.keyCode === 13){
-                document.getElementById("valider").click();
-             }
-          });
-          </script>
 
 </html>
